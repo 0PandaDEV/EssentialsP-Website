7deba30dc1fadf88e31b2d5b807a71d677d01151
Waradu <einfach_noan@outlook.de>
2022-10-25 16:23:19 +0200

    Remade Header Added more Links

--- script.js
+++
@@ -1,8 +0,0 @@
-var image = document.images[0]
-var bigImage = document.createElement("img")
-bigImage.onload = function() {
-  image.src = this.src
-}
-setTimeout(function() {
-  bigImage.src = "assets/home image.jpg";
-}, 50)

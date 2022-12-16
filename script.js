7f055e41d093a1395948c7986ac13bc496513995
PandaDEV <nils.ulmann@klangkonzept.ch>
2022-12-16 17:08:19 +0100

    moved colapable js code

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function reveal() {
     }
   }
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var colab = document.getElementsByClassName("colab");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active_colab");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
